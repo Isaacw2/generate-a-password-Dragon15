834a9daff228deeed2469de775c748e791f54256
isaac weisshaupt <147351308+Isaacw2@users.noreply.github.com>
2023-11-06 21:03:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,46 @@
 function generatePassword() {
-	var length = 8,
-		 charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+={}[]|?/><;:",
-		 password = "";
-   
-	for (var i = 0, n = charset.length; i < length; i++) {
-		 password += charset.charAt(Math.floor(Math.random() * n));
-	}
-   
-	document.getElementById("password").textContent = password;
-   }
+    let length = prompt('Choose a password length between 8 and 128 characters');
+    length = Math.floor(Math.random() * (128 - 8 + 1) + 8);
+    let password = '';
+    let lowerCase = confirm('Include lowercase letters?');
+    let upperCase = confirm('Include uppercase letters?');
+    let numeric = confirm('Include numeric characters?');
+    let special = confirm('Include special characters?');
 
+    if (lowerCase || upperCase || numeric || special) {
+      let typesCount = lowerCase + upperCase + numeric + special;
+      let options = {
+        lowerCase: 'abcdefghijklmnopqrstuvwxyz',
+        upperCase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        numeric: '0123456789',
+        special: '!@#$%^&*()_-+={}[]|?/.,:;<>',
+      };
+
+      for (let i = 0; i < length; i++) {
+        let typeIndex = Math.floor(Math.random() * typesCount);
+        let selectedType;
+
+        switch (typeIndex) {
+          case 0:
+            selectedType = options.lowerCase;
+            break;
+          case 1:
+            selectedType = options.upperCase;
+            break;
+          case 2:
+            selectedType = options.numeric;
+            break;
+          case 3:
+            selectedType = options.special;
+            break;
+        }
+
+        password += selectedType[Math.floor(Math.random() * selectedType.length)];
+      }
+    } else {
+      alert('Please select at least one character type');
+      return;
+    }
+
+    alert('Generated password: ' + password);
+ }
