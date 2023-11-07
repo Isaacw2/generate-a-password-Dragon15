03e141214954b4ac7763101a31860ac5787a200c
isaac weisshaupt <147351308+Isaacw2@users.noreply.github.com>
2023-11-06 20:49:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function generatePassword() {
+	var length = 8,
+		 charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+={}[]|?/><;:",
+		 password = "";
+   
+	for (var i = 0, n = charset.length; i < length; i++) {
+		 password += charset.charAt(Math.floor(Math.random() * n));
+	}
+   
+	document.getElementById("password").textContent = password;
+   }
 
